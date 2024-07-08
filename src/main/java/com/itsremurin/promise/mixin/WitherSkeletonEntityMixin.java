50556ccf756a71b5fff1ac06733f560393e894b2
Remurin <73508129+ItsRemurin@users.noreply.github.com>
2024-07-08 11:35:13 -0700

    Allow Wither Skeletons to Spawn with Armor

--- src/main/java/com/itsremurin/promise/mixin/WitherSkeletonEntityMixin.java
+++ src/main/java/com/itsremurin/promise/mixin/WitherSkeletonEntityMixin.java
@@ -1,11 +1,14 @@
 package com.itsremurin.promise.mixin;
 
+import net.minecraft.data.server.loottable.vanilla.VanillaEntityLootTableGenerator;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.mob.AbstractSkeletonEntity;
-import net.minecraft.entity.mob.SkeletonEntity;
 import net.minecraft.entity.mob.WitherSkeletonEntity;
-import net.minecraft.item.ItemConvertible;
+import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.math.random.Random;
@@ -13,8 +16,7 @@ import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(WitherSkeletonEntity.class)
 public abstract class WitherSkeletonEntityMixin extends AbstractSkeletonEntity {
@@ -28,7 +30,15 @@ public abstract class WitherSkeletonEntityMixin extends AbstractSkeletonEntity {
      */
     @Overwrite
     public void initEquipment(Random random, LocalDifficulty localDifficulty) {
-        super.initEquipment(random, localDifficulty);
-        this.equipStack(EquipmentSlot.MAINHAND, new ItemStack(random.nextFloat() < 0.25 ? Items.BOW : Items.STONE_SWORD));
+        this.equipAtChance(EquipmentSlot.CHEST, new ItemStack(Items.CHAINMAIL_CHESTPLATE), random);
+        this.equipAtChance(EquipmentSlot.FEET, new ItemStack(Items.CHAINMAIL_BOOTS), random);
+        this.equipStack(EquipmentSlot.MAINHAND, new ItemStack(random.nextFloat() > 0.25f ? Items.STONE_SWORD : Items.BOW));
+    }
+
+    @Unique
+    private void equipAtChance(EquipmentSlot slot, ItemStack stack, Random random) {
+        if (random.nextFloat() < 0.1f) {
+            this.equipStack(slot, stack);
+        }
     }
 }
